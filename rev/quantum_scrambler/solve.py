016e157ac355953405c594fc868ca6cf57066c53
monkey <spark61974@gmail.com>
2025-08-04 22:09:42 -0400

    finished quantum scrambler

--- rev/quantum_scrambler/solve.py
+++ rev/quantum_scrambler/solve.py
@@ -5,16 +5,17 @@ def main():
     PORT = 52453
     p = remote(HOST, PORT)
     input = p.recvall().decode()
+    #print(input)
     lists = eval(input)
-    print(f"flag len: {len(lists)*2}")
-    # we need the first and last hex char in each element of
-    # the lists outputted from eval
-
-    # im missing 2 characters since my current len is 30 not 32 so I need to manipualte the finals element in lists somehow, check source
+    #print(f"flag len: {len(lists)*2}")
+    
     flag = ""
-    for each in lists: 
+    for each in lists[:-2]: 
         flag += chr(int(each[0],16))
         flag += chr(int(each[-1],16))        
-        print(flag)
+    # bc they start at 2 for scrambling the last 2 elements we only take the first and not the first AND last char of
+    for each in lists[-2:]:
+        flag += chr(int(each[0],16))
+    print(flag)
 
 main()
